cca50eda9a33e5cff29dabc0296e129c183a62fb
Roy <Rouyeen@gmail.com>
2018-11-12 10:44:42 -0800

    Added Lat Lon to ECI conversion with some tests

--- kaos/algorithm/coord_conversion.py
+++ kaos/algorithm/coord_conversion.py
@@ -1,9 +1,12 @@
 from math import sqrt,sin,cos,pi,atan,tan
 from numpy import rad2deg,deg2rad
+from astropy import coordinates, units
+from astropy.time import Time
+from kaos.algorithm import Vector3D
 
 def lla_to_ecef(lat, lon, alt=0):
     """converts latitude, longitude, and altitude to earth-centered, earth-fixed (ECEF) Cartesian.
-    
+
     Args:
     lat = geodetic latitude (decimal degrees)
     lon = longitude (decimal degrees)
@@ -30,7 +33,7 @@ def lla_to_ecef(lat, lon, alt=0):
     # WGS84 ellipsoid constants:
     a = 6378137
     e = 8.1819190842622e-2
-    #decimal degrees to radians 
+    #decimal degrees to radians
     lat = lat/360*2*pi
     lon = lon/360*2*pi
     # intermediate calculation
@@ -61,3 +64,27 @@ def geod_to_geoc_lat(geod_lat_deg):
     geod_lat_rad =deg2rad(geod_lat_deg)
     geoc_lat_rad = atan(((1-flattening)**2)*tan(geod_lat_rad))
     return rad2deg(geoc_lat_rad)
+
+def lla_to_eci(lat, lon, alt=0, time_posix=946684800):
+    """
+    Converts geodetic lat,lon,alt, to a Cartesian vector in GCRS frame at the given time.
+
+    Args:
+    at = geodetic latitude (decimal degrees)
+    lon = longitude (decimal degrees)
+    alt = height above WGS84 ellipsoid (m)
+    time_posix= reference frame time
+
+    Returns:
+    Vector3D(x,y,z) such that:
+      x = GCRS X-coordinate (m)
+      y = GCRS Y-coordinate (m)
+      z = GCRS Z-coordinate (m)
+
+    Important Note: Unlike the rest of the software that uses J2000 FK5, the ECI frame used here is
+    GCRS; This can potentially introduce around 200m error for locations on surface of Earth.
+    """
+    posix_zero = Time(time_posix, format='unix')
+    loc_lla = coordinates.EarthLocation.from_geodetic(lon,lat,alt)
+    loc_eci = loc_lla.get_gcrs_posvel(posix_zero)
+    return Vector3D(loc_eci[0].x.value,loc_eci[0].y.value,loc_eci[0].z.value)
