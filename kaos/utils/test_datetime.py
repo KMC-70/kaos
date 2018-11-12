8fb077c0f8684359875fa763f3a74b146137724b
Yanruii <yanrayli@gmail.com>
2018-11-12 11:38:36 -0800

    Change the input error to a value error. Some other minor changes.

--- kaos/utils/test_datetime.py
+++ kaos/utils/test_datetime.py
@@ -1,19 +1,18 @@
 import datetime
 
 def UTC_to_Linux(time_string):
-    """The function's docstring"""
+    """This function takes a string input of the format '20181010T20:10:05.123' representing
+    'YYYYMMDDTHH:MM:SS.MS' and converts it to a linux timestamp. Note: the domain of time_string
+     should be between 19700101 and 20380119.
+    """
+    TIME_MIN = '19700101'
+    TIME_MAX = '20380119'
 
-    """This function takes a string input of the format 
-    '20181010T20:10:05.123' representing 'YYYYMMDDTHH:MM:SS.MS'
-    and converts it to a linux timestamp. Note: the domain of 
-    time_string should be between 19700101 and 20380119."""
-    dt = datetime.datetime.strptime\
-        (time_string, '%Y%m%dT%H:%M:%S.%f')
-    if (dt < datetime.datetime.strptime('19700101', '%Y%m%d') or
-            dt > datetime.datetime.strptime('20380109', '%Y%m%d')):
-        print("time_string out of range. Make sure it is between"
-              "19700101 and 20380119")
+    dt = datetime.datetime.strptime(time_string, '%Y%m%dT%H:%M:%S.%f')
+    if (dt < datetime.datetime.strptime(TIME_MIN, '%Y%m%d') or
+            dt > datetime.datetime.strptime(TIME_MAX, '%Y%m%d')):
+        raise ValueError('time_string out of range. Make sure it is between %s and %s'
+                         % (TIME_MIN, TIME_MAX))
         return None
-    ut = dt.timestamp()
+    return dt.timestamp()
 
-    return ut
