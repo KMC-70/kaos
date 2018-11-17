c5771705727e0a93e43f82ee90075d49d04342a4
Roy <Rouyeen@gmail.com>
2018-11-17 14:24:45 -0800

    Decoupled viewing cone tests form coord_conversion functions

--- test/algorithm/test_view_cone.py
+++ test/algorithm/test_view_cone.py
@@ -4,7 +4,7 @@ from random import randint
 
 from ddt import ddt, data
 
-from kaos.algorithm import view_cone, coord_conversion, TimeInterval, ViewConeError
+from kaos.algorithm import view_cone, TimeInterval, Vector3D, ViewConeError
 
 # TODO: move test constants to a unified location
 # Time constants for test environment:
@@ -15,12 +15,14 @@ J2000 = 946684800 # Jan 1st 1970 in POSIX
 class TestViewCone(unittest.TestCase):
     """ Test cases for viewing cone algorithm"""
     @data(
-        ((0, 0), (7.3779408317663437e+06, 4.9343382472754805e+04, 2.1445380156320367e+04),
+        (Vector3D(x=-1104185.9192367536, y=6281831.34325032, z=147.06403560447933),
+         (7.3779408317663437e+06, 4.9343382472754805e+04, 2.1445380156320367e+04),
          (-2.1365998990816905e+01, 2.2749470591161244e-01, 7.3501075690228217e+03),
          7378140*(1+1.8e-19), 2, 1.557161739571678e+05, 1.843521229948921e+05, 1.412700779528347e+05
          , 1.987982189908994e+05),
 
-        ((0, -110), (3.8947064924267233e+03, -3.1853237741789821e+03, -5.4020492601011592e+03),
+        (Vector3D(x=6280644.383697788, y=-1110917.500932147, z=149.53109223909593),
+         (3.8947064924267233e+03, -3.1853237741789821e+03, -5.4020492601011592e+03),
          (-5.6110588908929424e+06, -4.4103685540919630e+06, -1.9375720842113465e+06),
          7478140*(1+0.05), 0, 2.027181145241799e+04, 3.957976730797361e+04, -3.503052653228815e+03,
          6.335463139827675e+04)
@@ -34,9 +36,7 @@ class TestViewCone(unittest.TestCase):
         Values generated using: A Matlab implementation of viewing cone (using aerospace toolbox)
             which in turn was tested with STK
         """
-
-        site_eci = coord_conversion.lla_to_eci(test_data[0][0], test_data[0][1], 0, J2000)
-        t_1, t_2, t_3, t_4 = view_cone._view_cone_calc(site_eci, test_data[1], test_data[2],
+        t_1, t_2, t_3, t_4 = view_cone._view_cone_calc(test_data[0], test_data[1], test_data[2],
                                                        test_data[3], test_data[4])
 
         self.assertAlmostEqual(t_1, test_data[5], delta=5)
@@ -45,13 +45,15 @@ class TestViewCone(unittest.TestCase):
         self.assertAlmostEqual(t_4, test_data[8], delta=5)
 
     @data(
-        ((0, 0), (7.3779408317663437e+06, 4.9343382472754805e+04, 2.1445380156320367e+04),
+        (Vector3D(x=-1104185.9192367536, y=6281831.34325032, z=147.06403560447933),
+         (7.3779408317663437e+06, 4.9343382472754805e+04, 2.1445380156320367e+04),
          (-2.1365998990816905e+01, 2.2749470591161244e-01, 7.3501075690228217e+03),
          7378140*(1+1.8e-19), TimeInterval(J2000, J2000+ONE_DAY),
          [(J2000+1.202394298942655e+04, J2000+2.647003898543385e+04),
           (J2000+5.510598795010193e+04, J2000+6.955208395443503e+04)]),
 
-        ((0, -110), (3.8947064924267233e+03, -3.1853237741789821e+03, -5.4020492601011592e+03),
+        (Vector3D(x=6280644.383697788, y=-1110917.500932147, z=149.53109223909593),
+         (3.8947064924267233e+03, -3.1853237741789821e+03, -5.4020492601011592e+03),
          (-5.6110588908929424e+06, -4.4103685540919630e+06, -1.9375720842113465e+06),
          7478140*(1+0.05), TimeInterval(J2000, J2000+ONE_DAY),
          [(J2000, J2000+2.027181145241799e+04),
@@ -67,20 +69,21 @@ class TestViewCone(unittest.TestCase):
         Values generated using: A Matlab implementation of viewing cone (using aerospace toolbox)
             which in turn was tested with STK
         """
-        site_eci = coord_conversion.lla_to_eci(test_data[0][0], test_data[0][1], 0, J2000)
-        poi_list = view_cone.reduce_poi(site_eci, test_data[1], test_data[2], test_data[3],
-                                       test_data[4])
+        poi_list = view_cone.reduce_poi(test_data[0], test_data[1], test_data[2], test_data[3],
+                                        test_data[4])
         for answer, expected in zip(poi_list, test_data[5]):
             self.assertAlmostEqual(answer.start, expected[0], delta=5)
             self.assertAlmostEqual(answer.end, expected[1], delta=5)
 
     @data(
         # Test case with only 2 roots
-        ((40, 80), (6.8779541256529745e+06, 4.5999490750985817e+04, 1.9992074250214235e+04),
+        (Vector3D(x=-4892824.52928303, y=2505.6040609508723, z=4077844.5084031746),
+         (6.8779541256529745e+06, 4.5999490750985817e+04, 1.9992074250214235e+04),
          (-5.1646755701370530e+01, 5.3829730836383123e+03, 5.3826328640238344e+03),
          6878140*(1+1.8e-16), TimeInterval(J2000, J2000+ONE_DAY)),
         # Test case with no roots (always inside the viewing cone)
-        ((0, 0), (7.3779408317663465e+06, 4.9343382472754820e+04, 2.1445380156320374e+04),
+        (Vector3D(x=-1104185.9192367536, y=6281831.34325032, z=147.06403560447933),
+         (7.3779408317663465e+06, 4.9343382472754820e+04, 2.1445380156320374e+04),
          (-5.0830385351827260e+01, 7.3220252051302523e+03, 6.4023511402880990e+02),
          7378140*(1+1.8e-16), TimeInterval(J2000, J2000+ONE_DAY))
     )
@@ -93,13 +96,12 @@ class TestViewCone(unittest.TestCase):
         Values generated using: A Matlab implementation of viewing cone (using aerospace toolbox)
             which in turn was tested with STK
         """
-        site_eci = coord_conversion.lla_to_eci(test_data[0][0], test_data[0][1], 0, J2000)
         with self.assertRaises(ViewConeError):
-            view_cone.reduce_poi(site_eci, test_data[1], test_data[2], test_data[3], test_data[4])
+            view_cone.reduce_poi(test_data[0], test_data[1], test_data[2], test_data[3],
+                                 test_data[4])
 
     def test_reduce_poi_input_error(self):
         """Tests whether reduce_poi can detect improper POI"""
-
         # Create an improperly ordered POI
         small = randint(1, 100000000)
         big = randint(1, 100000000)
