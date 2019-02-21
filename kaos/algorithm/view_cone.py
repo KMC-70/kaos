273e1b2133afda65ceacae24f8ab2bc6e847bd0c
Roy <Rouyeen@gmail.com>
2019-02-20 16:41:23 -0800

    Fix viewing cone algorithm

--- kaos/algorithm/view_cone.py
+++ kaos/algorithm/view_cone.py
@@ -1,83 +1,64 @@
 """Implementation of Viewing cone algorithm"""
-from math import asin, atan, sqrt, sin, cos, pi
-import numpy as np
-from numpy import cross
-from numpy.linalg import norm
 
-from ..constants import SECONDS_PER_DAY, ANGULAR_VELOCITY_EARTH, THETA_NAUGHT
+from __future__ import division
+import mpmath as mp
+from numpy import cross
+
+from .coord_conversion import geod_to_geoc_lat
+from ..constants import SECONDS_PER_DAY, ANGULAR_VELOCITY_EARTH, THETA_NAUGHT, EARTH_RADIUS
 from ..tuples import TimeInterval
 from ..errors import ViewConeError
 
 
-# pylint: disable=invalid-name,too-many-locals
-def cart2sp(x, y, z):
-    """Converts data from cartesian coordinates into spherical.
-
+def reduce_poi(site_lat_lon, sat_pos, sat_vel, q_magnitude, poi):
+    """Performs a series of viewing cone calculations and shrinks the input POI
     Args:
-        x (scalar or array_like): X-component of data.
-        y (scalar or array_like): Y-component of data.
-        z (scalar or array_like): Z-component of data.
-
+      site_eci(Vector3D) = site location in ECI at the start of POI
+      sat_pos(Vector3D) = position of satellite (at an arbitrary time)
+      sat_vel(Vector3D) = velocity of satellite (at the same arbitrary time as sat_pos)
+      q_magnitude(int) = maximum orbital radius
+      poi(TimeInterval) = period of interest
     Returns:
-        Tuple (r, theta, phi) of data in spherical coordinates.
-    """
-    x = np.asarray(x)
-    y = np.asarray(y)
-    z = np.asarray(z)
-    scalar_input = False
-    if x.ndim == 0 and y.ndim == 0 and z.ndim == 0:
-        x = x[None]
-        y = y[None]
-        z = z[None]
-        scalar_input = True
-    r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
-    theta = np.arcsin(z / r)
-    phi = np.arctan2(y, x)
-    if scalar_input:
-        return (r.squeeze(), theta.squeeze(), phi.squeeze())
-    return (r, theta, phi)
-
-
-def reduce_poi(site_eci, sat_pos, sat_vel, q_magnitude, poi):
-    """Performs a series of viewing cone calculations and shrinks the input POI.
-
-    Args:
-        site_eci (Vector3D): site location in ECI at the start of POI
-        sat_pos (Vector3D): position of satellite (at an arbitrary time)
-        sat_vel (Vector3D): velocity of satellite (at the same arbitrary time as sat_pos)
-        q_magnitude (int): maximum orbital radius
-        poi (TimeInterval): period of interest
-
-    Returns:
-        A list of TimeIntervals that the orbit is inside viewing cone.
-
+      list of TimeIntervals that the orbit is inside viewing cone
     Raises:
-        ValueError: on unexpected input
-        ViewConeError: on inconclusive result from Viewing cone
+      ValueError: on unexpected input
+      ViewConeError: on inconclusive result from Viewing cone
     """
+
+    site_geoc_lat = geod_to_geoc_lat(site_lat_lon[0])
+
+    GMT_sidereal_angle = (poi.start - mp.mpf(946728000))*(360/SECONDS_PER_DAY) + mp.mpf('280.46062')
+    site_lon = GMT_sidereal_angle + site_lat_lon[1]
+    site_lon = mp.floor(site_lon)%360 + (site_lon - mp.floor(site_lon))
+
+    if site_lon > 180:
+        site_lon -= 360
+
     if poi.start > poi.end:
         raise ValueError("poi.start is after poi.end")
 
-    # Tracking how much of the POI has been processed
-    cur_end = poi.start
-    # Estimate of maximum m
-    expected_final_m = ((poi.end - poi.start) / SECONDS_PER_DAY) + 1
-
+    # Maximum m
+    expected_final_m = mp.ceil((poi.end - poi.start)/(24*60*60))
     # Find the intervals to cover the input POI
     interval_list = []
     m = 0
-    while (cur_end < poi.end) and (m < expected_final_m):
+    while m < expected_final_m:
         try:
-            t_1, t_2, t_3, t_4 = _view_cone_calc(site_eci, sat_pos, sat_vel, q_magnitude, m)
+            t_1, t_2, t_3, t_4 = _view_cone_calc(site_geoc_lat, site_lon, sat_pos, sat_vel, q_magnitude, m)
             # Validate the intervals
-            if (t_3 > t_1) or (t_2 > t_4):
-                # Unexpected order of times
-                raise ViewConeError("Viewing Cone internal error")
-            # Add intervals to the list
-            interval_list.append(TimeInterval(poi.start + t_3, poi.start + t_1))
-            interval_list.append(TimeInterval(poi.start + t_2, poi.start + t_4))
+            if (t_3 < t_1):
+                interval_list.append(TimeInterval(poi.start+t_3, poi.start+t_1))
+            else:
+                interval_list.append(TimeInterval(poi.start+t_3, poi.start+(m+1)*24*60*60))
+                interval_list.append(TimeInterval(poi.start+m*24*60*60, poi.start+t_1))
+
+            if (t_2 < t_4):
+                interval_list.append(TimeInterval(poi.start+t_2, poi.start+t_4))
+            else:
+                interval_list.append(TimeInterval(poi.start+t_2, poi.start+(m+1)*24*60*60))
+                interval_list.append(TimeInterval(poi.start+m*24*60*60, poi.start+t_4))
             m += 1
-            cur_end = poi.start + t_4
+
         except ValueError:
             # The case were the formulas have less than 4 roots
             raise ViewConeError("Unsupported viewing cone and orbit configuration.")
@@ -85,16 +66,13 @@ def reduce_poi(site_eci, sat_pos, sat_vel, q_magnitude, poi):
     # Adjusting the intervals to fit inside the input POI and return
     return _trim_poi_segments(interval_list, poi)
 
-
 def _trim_poi_segments(interval_list, poi):
     """Semi-private: Adjusts list of intervals so that all intervals fit inside the poi
-
         Args:
-            interval_list (list of TimeIntervals): the intervals to be trimmed
-            poi (TimeInterval): period of interest, reference for trimming
-
+          interval_list(list of TimeIntervals) = the interval to be trimmed
+          poi(TimeInterval) = period of interest, reference for trimming
         Returns:
-            List of TimeIntervals that fit inside the poi
+          List of TimeIntervals that fit inside the poi
     """
     ret_list = []
     for interval in interval_list:
@@ -111,54 +89,76 @@ def _trim_poi_segments(interval_list, poi):
     return ret_list
 
 
-def _view_cone_calc(site_eci, sat_pos, sat_vel, q_magnitude, m):
+def _view_cone_calc(lat_geoc, lon_geoc, sat_pos, sat_vel, q_magnitude, m):
     """Semi-private: Performs the viewing cone visibility calculation for the day defined by m.
-
-    This function is based on a paper titled "rapid satellite-to-site visibility determination
+    Note: This function is based on a paper titled "rapid satellite-to-site visibility determination
     based on self-adaptive interpolation technique"  with some variation to account for interaction
     of viewing cone with the satellite orbit.
-
     Args:
-        site_eci (Vector3D): site location in ECI at the start of POI
-        sat_pos (Vector3D): position of satellite (at the same time as sat_vel)
-        sat_vel (Vector3D): velocity of satellite (at the same time as sat_pos)
-        q_magnitude (int): maximum orbital radius
-
+      lat_geoc(mpf) = site location in degrees at the start of POI
+      lon_geoc(mpf) = site location in degrees at the start of POI
+      sat_pos(Vector3D) = position of satellite (at the same time as sat_vel)
+      sat_vel(Vector3D) = velocity of satellite (at the same time as sat_pos)
+      q_magnitude(int) = maximum orbital radius
     Returns:
-        Returns 4 numbers representing times at which the orbit is tangent to the viewing cone,
-
+      Returns 4 numbers representing times at which the orbit is tangent to the viewing cone,
     Raises:
-        ValueError: if any of the 4 formulas has a complex answer. This happens when the orbit and
-            viewing cone do not intersect or only intersect twice.
-
-    Note:
-        With more analysis it should be possible to find a correct interval even in the case
-        where there are only two intersections but this is beyond the current scope of the project.
+      ValueError: if any of the 4 formulas has a complex answer. This happens when the orbit and
+      viewing cone do not intersect or only intersect twice.
+      Note: With more analysis it should be possible to find a correct interval even in the case
+      where there are only two intersections but this is beyond the current scope of the project.
     """
 
-    # Get geocentric angles from site ECI
-    r_site_magnitude, lat_geoc, lon_geoc = cart2sp(site_eci.x, site_eci.y, site_eci.z)
+    lat_geoc = (lat_geoc*mp.pi)/180
+    lon_geoc = (lon_geoc*mp.pi)/180
 
     # P vector (also referred  to as orbital angular momentum in the paper) calculations
-    p_unit_x, p_unit_y, p_unit_z = cross(sat_pos, sat_vel) / (norm(sat_pos) * norm(sat_vel))
+    p_unit_x, p_unit_y, p_unit_z = cross(sat_pos, sat_vel) / (mp.norm(sat_pos) * mp.norm(sat_vel))
 
     # Formulas from paper:
-    # Note: each Txxx represents an intersection between viewing cone and the orbit
-    gamma = THETA_NAUGHT + asin((r_site_magnitude * sin((pi / 2) + THETA_NAUGHT)) / q_magnitude)
-    tin = ((1 / ANGULAR_VELOCITY_EARTH) * (asin((cos(gamma) - (p_unit_z * sin(lat_geoc))) /
-            (sqrt((p_unit_x ** 2) + (p_unit_y ** 2)) * cos(lat_geoc)))
-            - lon_geoc - atan(p_unit_x / p_unit_y) + 2 * pi * m))
-    tout = ((1 / ANGULAR_VELOCITY_EARTH) * (pi - asin((cos(gamma) - (p_unit_z * sin(lat_geoc))) /
-            (sqrt((p_unit_x ** 2) + (p_unit_y ** 2)) * cos(lat_geoc)))
-            - lon_geoc - atan(p_unit_x / p_unit_y) + 2 * pi * m))
+    gamma = THETA_NAUGHT + mp.asin((EARTH_RADIUS * mp.sin((mp.pi / 2) + THETA_NAUGHT)) / q_magnitude)
+    gamma2 = mp.pi - gamma
 
-    # Second set
-    gamma2 = pi - gamma
-    tin_2 = ((1 / ANGULAR_VELOCITY_EARTH) * (asin((cos(gamma2) - (p_unit_z * sin(lat_geoc))) /
-            (sqrt((p_unit_x ** 2) + (p_unit_y ** 2)) * cos(lat_geoc)))
-            - lon_geoc - atan(p_unit_x / p_unit_y) + 2 * pi * m))
-    tout_2 = ((1 / ANGULAR_VELOCITY_EARTH) * (pi - asin((cos(gamma2) - (p_unit_z * sin(lat_geoc))) /
-            (sqrt((p_unit_x ** 2) + (p_unit_y ** 2)) * cos(lat_geoc)))
-            - lon_geoc - atan(p_unit_x / p_unit_y) + 2 * pi * m))
+    arctan_term = mp.atan2(p_unit_x , p_unit_y)
+    arcsin_term = lambda g:(mp.asin((mp.cos(g) - p_unit_z * mp.sin(lat_geoc)) /
+                            (mp.sqrt((p_unit_x ** 2) + (p_unit_y ** 2)) * mp.cos(lat_geoc))))
 
-    return tin, tout, tin_2, tout_2
+    arcsin_term_gamma = arcsin_term(gamma)
+    arcsin_term_gamma2 = arcsin_term(gamma2)
+
+    angle_1 = (arcsin_term_gamma - lon_geoc - arctan_term + 2 * mp.pi * m)
+    angle_2 = (mp.pi - arcsin_term_gamma - lon_geoc - arctan_term + 2 * mp.pi * m)
+    angle_3 = (arcsin_term_gamma2 - lon_geoc - arctan_term + 2 * mp.pi * m)
+    angle_4 = (mp.pi - arcsin_term_gamma2 - lon_geoc - arctan_term + 2 * mp.pi * m)
+
+    # Map all angles to 0 to 2*pi
+    while angle_1 < 0:
+        angle_1 += 2*mp.pi
+    while angle_1 > 2*mp.pi:
+        angle_1 -= 2*mp.pi
+
+    while angle_2 < 0:
+        angle_2 += 2*mp.pi
+    while angle_2 > 2*mp.pi:
+        angle_2 -= 2*mp.pi
+
+    while angle_3 < 0:
+        angle_3 += 2*mp.pi
+    while angle_3 > 2*mp.pi:
+        angle_3 -= 2*mp.pi
+
+    while angle_4 < 0:
+        angle_4 += 2*mp.pi
+    while angle_4 > 2*mp.pi:
+        angle_4 -= 2*mp.pi
+
+    time_1 = (1 / ANGULAR_VELOCITY_EARTH) * angle_1
+    time_2 = (1 / ANGULAR_VELOCITY_EARTH) * angle_2
+    time_3 = (1 / ANGULAR_VELOCITY_EARTH) * angle_3
+    time_4 = (1 / ANGULAR_VELOCITY_EARTH) * angle_4
+
+    # Check for complex answers
+    if [x for x in [time_1, time_2, time_3, time_4] if not isinstance(x, mp.mpf)]:
+        raise ValueError()
+
+    return time_1, time_2, time_3, time_4
--- kaos/constants.py
+++ kaos/constants.py
@@ -3,12 +3,15 @@
 Author: Team KMC-70
 """
 
+import mpmath as mp
+
 # WGS84 ellipsoid constants
 ELLIPSOID_A = 6378137
 ELLIPSOID_E = 8.1819190842622e-2
 
 # Viewing cone calculation constants
-ANGULAR_VELOCITY_EARTH = 7.2921159e-5  # at equator
-SECONDS_PER_DAY = 86164  # 23:56:04, in seconds
-THETA_NAUGHT = 0  # visibility threshold
-J2000 = 946684800  # Jan 1st 1970 in POSIX
+ANGULAR_VELOCITY_EARTH = 7.2921159e-5
+EARTH_RADIUS = 6371008.8 # earth's radius (meters)
+SECONDS_PER_DAY = 23*60*60 + 56*60 + mp.mpf('4.0989')
+THETA_NAUGHT = 0  # visibility threshold (Rad)
+J2000 = 946684800  # Jan 1st 2000 @ 00:00 in POSIX (not 12:00)
