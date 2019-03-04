244fa1d58039e44acc7378d7aa3680c8cbd40acf
Zeyad Tamimi <zeyadtamimi@outlook.com>
2019-03-04 12:14:51 -0800

    removed pdb

--- kaos/__init__.py
+++ kaos/__init__.py
@@ -76,7 +76,6 @@ def create_app(config="settings.cfg"):
     def method_not_allowed(error):
         response = jsonify(reason=str(error))
         response.status_code = error.code
-        import pdb; pdb.set_trace()
         return response
 
     @app.errorhandler(APIError)
